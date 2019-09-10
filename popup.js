b58e24f366e5f1281705f40fd7d65a6a8c66cd7e
Cherie Young <youncher@oregonstate.edu>
2019-09-10 00:21:42 -0700

    Updated background to store user input (time)

--- popup.js
+++ popup.js
@@ -1,7 +1,24 @@
+let startStop = document.getElementById('startStop');
+
+startStop.onclick = () => {
+	let hours = document.getElementById('hours').value || '0';
+	let minutes = document.getElementById('minutes').value || '0';
+	let seconds = document.getElementById('seconds').value || '0';
+
+	chrome.runtime.sendMessage({
+		type: "start",
+		data: {
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds
+		}}, (response) => {
+		chrome.extension.getBackgroundPage().console.log(response); // TODO handle response
+	});
+};
+
 let refreshNow = document.getElementById('refreshNow');
 
-refreshNow.onclick = (element) => {
-	let color = element.target.value;
+refreshNow.onclick = () => {
 	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
 		var code = 'window.location.reload();';
 		chrome.tabs.executeScript(
